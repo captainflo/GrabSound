fbf85193ed340b1b6da618e31889515df24629ab
Florian Lahitte <lahitte.florian@gmail.com>
2020-05-05 13:03:57 -0400

    fix connect

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,6 +1,6 @@
 // prod.js - production keys here!!!
 module.exports = {
-  siteUrl: 'https://grabsound.herokuapp.com',
+  siteUrl: 'https://soundgrab.herokuapp.com',
   cloudinaryClientName: process.env.CLOUDINARY_NAME,
   cloudinaryClientID: process.env.CLOUDINARY_CLIENT_ID,
   cloudinaryClientSecret: process.env.CLOUDINARY_CLIENT_SECRET,
